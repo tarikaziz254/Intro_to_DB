2867298622e8f8e7eaea7db9da469f01b6f7b7a8
tarik <tarikaziz2242@gmail.com>
2024-07-27 04:51:38 -0700

    change the code from using python to sql

--- task_2.sql
+++ task_2.sql
@@ -1,73 +1,37 @@
-"""
-This program tables for database alx_book_store,
-it has books,authors,customers,orders and order_details
-"""
+CREATE TABLE Books (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(130),
+    author_id INT,
+    price DOUBLE,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
 
-import mysql.connector
+CREATE TABLE Authors(
 
-#connecting with the database
-mydb = mysql.connector.connect(
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR (215)
+);
 
-    host = "localhost",
-    user = "root",
-    password = "22427843Tk!!?",
-    database = "alx_book_store"
-)
+CREATE TABLE Customers (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    customer_email VARCHAR(215),
+    address TEXT
+);
 
-mycursor = mydb.cursor()
+CREATE TABLE Orders (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
 
-# creating table books
-mycursor.execute("""CREATE TABLE IF NOT EXISTS books(
-                 
-                 book_id INT AUTO_INCREMENT PRIMARY KEY,
-                 title VARCHAR (130),
-                 author_id INT,
-                 price DOUBLE,
-                 publication_date DATE,
-                 FOREIGN KEY(author_id) REFERENCES authors(author_id)
-)
-""")
-print("successfully created")
-
-#author table
-mycursor.execute("""CREATE TABLE IF NOT EXISTS authors(
-                 
-                 author_id INT AUTO_INCREMENT PRIMARY KEY,
-                 author_name VARCHAR (215)
-)
-""")
-print("successfully created")
-
-#customers table
-mycursor.execute("""CREATE TABLE IF NOT EXISTS customers(
-                 
-                 customers_id INT AUTO_INCREMENT PRIMARY KEY,
-                 customer_name VARCHAR (215),
-                 email VARCHAR (215),
-                 address TEXT
-)
-""")
-
-#orders table
-mycursor.execute("""CREATE TABLE IF NOT EXISTS orders(
-                 
-                 order_id INT AUTO_INCREMENT PRIMARY KEY,
-                 customers_id INT,
-                 order_date DATE,
-                 FOREIGN KEY (customers_id) REFERENCES customers(customers_id)             
-)
-""")
-print("successfully created")
-
-#Order_details
-mycursor.execute("""CREATE TABLE IF NOT EXISTS order_details(
-                 
-                 orderdetail_id INT AUTO_INCREMENT PRIMARY KEY,
-                 order_id INT,
-                 book_id INT,
-                 quantity DOUBLE,
-                 FOREIGN KEY (order_id) REFERENCES orders(order_id),
-                 FOREIGN KEY (book_id) REFERENCES books(book_id)               
-)
-""")
-print("successfully created")
+CREATE TABLE Order_Details (
+    order_details_id INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
